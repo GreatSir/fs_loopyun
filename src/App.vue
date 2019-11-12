8ddd3d58067c650ff19f952a6443bbb44c6fe45e
xulongcai <792756411@qq.com>
2019-11-12 23:29:51 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <van-nav-bar
+    title="范特西体育"
+    fixed
+    class="nb"
+    />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: black;
+  margin-top: 60px;
+}
+.nb{
+  color: #ffffff;
+  background-color: black;
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+<van-button type="primary">主要按钮</van-button>
+    <van-swipe :autoplay="3000">
+  <van-swipe-item v-for="(image, index) in images" :key="index">
+    <img v-lazy="image" />
+  </van-swipe-item>
+</van-swipe>
+</div>
+</template>
+<script>
+export default {
+    name: 'Index',
+    data(){
+        return {
+ images: [
+        'https://img.yzcdn.cn/vant/apple-1.jpg',
+        'https://img.yzcdn.cn/vant/apple-2.jpg'
+      ]
+        }
+    }
+}
+</script>
